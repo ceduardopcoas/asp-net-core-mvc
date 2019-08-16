dbb55e02b6d51c376a777543359fb50fcb942c3e
Carlos Eduardo Pizi Coas <ceduardopcoas@gmail.com>
2019-08-16 08:09:01 -0400

    encerrando classes

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -1,31 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+
 
 namespace SalesWebMvc.Models
 {
     public class Seller
     {
 
-        public int id { get; set; }
-        public string name { get; set; }
-        public DateTime birthDate { get; set; }
-        public double baseSalary { get; set; }
-        
-        public void addSales()
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDate { get; set; }
+        public double BaseSalary { get; set; }
+        public Department Department { get; set; }
+        public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
+
+        public Seller()
         {
 
         }
 
-        public void removeSales()
+        public Seller(int id, string name, string email, DateTime birthDate, double baseSalary, Department department)
         {
-
+            Id = id;
+            Name = name;
+            Email = email;
+            BirthDate = birthDate;
+            BaseSalary = baseSalary;
+            Department = department;
         }
 
-        public double totalSales()
+        public void addSales(SalesRecord sr)
         {
-            return 10;
+            Sales.Add(sr);
+        }
+
+        public void removeSales(SalesRecord sr)
+        {
+            Sales.Remove(sr);
+        }
+
+        public double totalSales(DateTime initial, DateTime final)
+        {
+            return Sales.Where(sr => sr.Date >= initial && sr.Date <= final).Sum(sr => sr.Amount);
         }
 
 
